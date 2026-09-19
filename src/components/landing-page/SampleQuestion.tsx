3336e302615f7146ff5936f37b580ba93a4536c3
Manish Goyal <manishgoyal5683@gmail.com>
2026-09-19 11:43:30 +0530

    sample ques works bare min

--- src/components/landing-page/SampleQuestion.tsx
+++ src/components/landing-page/SampleQuestion.tsx
@@ -1,9 +1,24 @@
 import { heading, sampleQuestion, sampleQuestionTopic } from "#/content/landing-page/sampleQuestion";
+import { useState } from "react";
 import { Button } from "../ui/button";
 
 export function SampleQuestion()
 {
-    
+    const [selectedOption, setSelectedOption] = useState<number>(0);
+    const [isCorrect, setIsCorrect] = useState<boolean | undefined>(undefined);
+    function revealRealAnswer()
+    {
+        if(sampleQuestion.correctOptions.find((correctOption) => correctOption === selectedOption))
+        {
+            //play some nice sound?
+            setIsCorrect(true);
+        }
+        else
+        {
+            //play some bad sound?
+            setIsCorrect(false);
+        }
+    }
     return(
         <section className="bg-card p-5 flex flex-col gap-4">
             <h2 className="font-bold text-2xl md:text-3xl">{heading}</h2>
@@ -22,12 +37,32 @@ export function SampleQuestion()
                     {
                         sampleQuestion.options.map((option, idx) =>
                             <li key={idx}>
-                                <Button variant={"outline"} className={`whitespace-normal h-auto text-left justify-start py-2`}>
+                                <Button
+                                    variant={"outline"}
+                                    onClick={() => setSelectedOption(idx+1)}
+                                    className=
+                                        {`
+                                            whitespace-normal h-auto text-left justify-start py-2
+                                            ${selectedOption === idx+1 ? "bg-primary/40": ""}
+                                            ${
+                                                isCorrect === undefined
+                                                ?
+                                                null
+                                                :
+                                                sampleQuestion.correctOptions.find((correctOption) => correctOption === idx+1)
+                                                ?
+                                                "bg-green-500/50"
+                                                :
+                                                "bg-destructive/50"
+                                            }
+                                        `}
+                                >
                                     {option}
                                 </Button>
                             </li>)
                     }
                 </ol>
+                <Button variant={"default"} onClick={revealRealAnswer}>Submit</Button>
             </div>
         </section>
     )
