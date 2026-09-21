7b25bf4d330c03cb344f1b8da3ec19e88de49da3
Manish Goyal <manishgoyal5683@gmail.com>
2026-09-22 02:45:40 +0530

    basic reusable form done, but need some serious bug fixes

--- src/components/landing-page/RequestingNewExams.tsx
+++ src/components/landing-page/RequestingNewExams.tsx
@@ -1,14 +1,25 @@
 //Content:
 import {heading} from "#/content/landing-page/requestingNewExams";
+import z from "zod";
 //Components:
 import { SimpleForm } from "../shared/SimpleForm";
 
 export function RequestingNewExams()
 {
+    const formFields =
+        [
+            {
+                name: "First Name",
+                type: "input",
+                defaultValue: "",
+                placeholder: "John",
+                validator: z.string().min(2, "Please enter at-least two chars")
+            }
+        ];
     return (
         <section className="bg-accent/25 px-5 py-10 flex flex-col justify-center items-center text-center gap-10">
             <h2 className="text-3xl font-semibold">{heading}</h2>
-            <SimpleForm/>
+            <SimpleForm formFields={formFields} submitButtonText="Request New Exam" onSubmit={(values) => console.log(values)}/>
         </section>
     );
 }
--- src/components/shared/SimpleForm.tsx
+++ src/components/shared/SimpleForm.tsx
@@ -1,61 +1,67 @@
 //Dependencies
 import {useForm} from "@tanstack/react-form";
-import {z} from "zod";
-//Components:
 import {Button} from "../ui/button";
 
-
+//Types:
 export type SimpleFormProps =
     {
-        
+        formFields:
+            {
+                name: string,
+                type: "input" | "email" | "tel",
+                defaultValue?: string,
+                placeholder?: string,
+                validator: any //zod schema
+            }[],
+        submitButtonText: string
+        onSubmit: any, //fn that takes all the values in the form...
     };
-export function SimpleForm({})
+
+//Component:
+export function SimpleForm({formFields, submitButtonText, onSubmit}: SimpleFormProps)
 {
     const simpleForm = useForm(
         {
-            defaultValues: {firstName: "", lastName: "", examName: "", rangeOfYears: ""},
-            onSubmit: ({value}) => console.log("submitted with: ", value),
+            defaultValues: formFields.map((field => field.defaultValue && "")),
+            onSubmit: onSubmit
         });
     return (
         <form onSubmit={(e) => {e.preventDefault(); e.stopPropagation(); simpleForm.handleSubmit()}} className="flex flex-col gap-2">
-            <simpleForm.Field
-                name="firstName"
-                validators={
-                    {
-                        onChange:
-                            z.string()
-                            .min(2, "First Name must be at-least two chars")
-                    }}
-            >
-                {
-                    (field) => (
-                        <div className="">
-                            <div className="flex gap-2 justify-center items-center">
-                            <label htmlFor={field.name}>First Name:</label>
-                            <input
-                                type="text"
-                                id={field.name}
-                                name={field.name}
-                                value={field.state.value}
-                                onBlur={field.handleBlur}
-                                onChange={(e) => field.handleChange(e.target.value)}
-                                placeholder="John"
-                                className="border p-1"
-                            />
-                            </div>
-                            {
-                                field.state.meta.isTouched
-                                &&
-                                field.state.meta.errors.length
-                                ?
-                                field.state.meta.errors.map(err => <span className="text-xs text-destructive">{err?.message}</span>)
-                                :
-                                null
-                            }
-                        </div>)
-                }
-            </simpleForm.Field>
-            <Button variant={"default"} type="submit">Request for a New Exam</Button>
+            {
+                formFields.map(({name, type, placeholder, validator}, idx) => (
+                    <simpleForm.Field key={idx} name={name} validators={validator}>
+                        {
+                            (field) => (
+                                <>
+                                    <div className="flex gap-2 justify-center items-center">
+                                        <label htmlFor={name}>{name}</label>
+                                        <input
+                                            type={type}
+                                            name={field.name}
+                                            id={field.name}
+                                            value={field.state.value}
+                                            onBlur={field.handleBlur}
+                                            onChange={(e) => field.handleChange(e.target.value)}
+                                            placeholder={placeholder}
+                                            className="text-base border p-1"
+                                        />
+                                    {
+                                        field.state.meta.isTouched
+                                        &&
+                                        field.state.meta.errors.length
+                                        ?
+                                        field.state.meta.errors.map((err, idx) => <span key={idx} className="text-xs text-destructive">{err?.message}</span>)
+                                        :
+                                        null
+                                    }
+                                    </div>
+                                </>
+                            )
+                        }
+                    </simpleForm.Field>
+                ))
+            }
+            <Button variant={"default"} type="submit">{submitButtonText}</Button>
         </form>
     );
 }
